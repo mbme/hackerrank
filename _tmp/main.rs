4659758fa6891884d4db6ee987ebbb4194ee31e3
mbme <stribog.ua@gmail.com>
2016-03-13 22:19:00 +0200

    one more helper function

--- _tmp/main.rs
+++ _tmp/main.rs
@@ -16,6 +16,10 @@ fn must_parse_arr<T> (s: String) -> Vec<T> where T: std::str::FromStr {
     s.split_whitespace().map(|item| must_parse(item.to_string())).collect()
 }
 
+fn string_of (c: char, n: u32) -> String {
+    (0..n).map(|_| c).collect::<String>()
+}
+
 fn main() {
     let num_str_1 = readline();
     let num_str_2 = readline();
