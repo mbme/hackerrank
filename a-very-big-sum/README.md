19b25597d1e5c15cd4c38bc536e2d45e6817950b
mbme <stribog.ua@gmail.com>
2016-03-13 16:15:57 +0200

    third contest

---
+++ a-very-big-sum/README.md
@@ -0,0 +1,4 @@
+# Hackerrank contest [https://www.hackerrank.com/challenges/a-very-big-sum](https://www.hackerrank.com/challenges/a-very-big-sum)
+
+Problem statement [tmp/statement.pdf](tmp/statement.pdf).
+
---
+++ a-very-big-sum/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+
+fn readline () -> String {
+    let mut input_str = String::new();
+
+    io::stdin().read_line(&mut input_str).ok().expect("read error");
+
+    input_str
+}
+
+fn must_parse<T> (num_str: String) -> T where T: std::str::FromStr {
+    num_str.trim().parse().ok().expect("parse error")
+}
+
+fn must_parse_arr<T> (s: String) -> Vec<T> where T: std::str::FromStr {
+    s.split_whitespace().map(|item| must_parse(item.to_string())).collect()
+}
+
+fn main() {
+    let size : i32 = must_parse(readline());
+    let items: Vec<i64> = must_parse_arr(readline());
+
+    assert_eq!(size, items.len() as i32);
+
+    println!("{}", items.iter().fold(0, |acc, &x| acc + x));
+}
