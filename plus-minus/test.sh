d1adec7840d3bcd19f5a990eb9b80ac2d3f6b53f
mbme <stribog.ua@gmail.com>
2016-03-13 23:07:36 +0200

    debug mode for the test script

--- plus-minus/test.sh
+++ plus-minus/test.sh
@@ -22,12 +22,17 @@ for INPUT_FILE in tmp/input/input*.txt; do
     echo "Testing with $INPUT_FILE"
     rm -f result.txt # ignore error if file is missing
 
-    # 2) run with input from the file
-    ./main < "$INPUT_FILE" > result.txt
+    if [ "$1" == "debug" ]; then
+        ./main < "$INPUT_FILE"
+    else
+        # 2) run with input from the file
+        ./main < "$INPUT_FILE" > result.txt
 
-    name=${INPUT_FILE##*/} # input00.txt
-    id=${name#input} # 00.txt
+        name=${INPUT_FILE##*/} # input00.txt
+        id=${name#input} # 00.txt
 
-    # 3) compare outputs
-    compare result.txt "tmp/output/output$id"
+        # 3) compare outputs
+        compare result.txt "tmp/output/output$id"
+    fi
+
 done
