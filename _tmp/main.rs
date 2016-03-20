338c137f8dec7b41bd4d1905105133bb301262e8
mbme <stribog.ua@gmail.com>
2016-03-20 17:58:09 +0200

    helpers improvements

--- _tmp/main.rs
+++ _tmp/main.rs
@@ -12,12 +12,16 @@ fn read_digits_matrix (n: usize) -> Vec<Vec<char>> {
     (0..n).map(|_| readline().trim().chars().collect::<Vec<char>>()).collect()
 }
 
-fn must_parse<T> (num_str: String) -> T where T: std::str::FromStr {
+fn must_parse<T> (num_str: &str) -> T where T: std::str::FromStr {
     num_str.trim().parse().ok().expect("parse error")
 }
 
-fn must_parse_arr<T> (s: String) -> Vec<T> where T: std::str::FromStr {
-    s.split_whitespace().map(|item| must_parse(item.to_string())).collect()
+fn must_parse_arr<T> (s: &str) -> Vec<T> where T: std::str::FromStr {
+    s.split_whitespace().map(|item| must_parse(item)).collect()
+}
+
+fn num_arr_to_string (n: &Vec<u64>) -> String {
+    n.iter().map(|d| d.to_string()).collect::<Vec<String>>().join(" ")
 }
 
 fn string_of (c: char, n: u32) -> String {
@@ -25,11 +29,5 @@ fn string_of (c: char, n: u32) -> String {
 }
 
 fn main() {
-    let num_str_1 = readline();
-    let num_str_2 = readline();
-
-    let num_1 : i32 = must_parse(num_str_1);
-    let num_2 : i32 = must_parse(num_str_2);
-
-    println!("{}", num_1 + num_2);
+    
 }
